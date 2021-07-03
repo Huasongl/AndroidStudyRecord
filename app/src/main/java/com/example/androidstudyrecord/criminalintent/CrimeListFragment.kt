064993056e7dd3055a4ad74dc93e57ef2b01e736
yaohua <1436385860@qq.com>
2021-07-04 06:21:31 +0800

    Implement different view

--- app/src/main/java/com/example/androidstudyrecord/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/androidstudyrecord/criminalintent/CrimeListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -22,14 +23,39 @@ class CrimeListFragment:Fragment() {
     private val crimeListViewModel:CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
     }
-    private inner class CrimeHolder(view:View):RecyclerView.ViewHolder(view){
-        val titleTextView:TextView = itemView.findViewById(R.id.crime_title)
-        val dateTextView:TextView=itemView.findViewById(R.id.crime_date)
+    private inner class CrimeHolder(view:View):RecyclerView.ViewHolder(view),View.OnClickListener{
+        private lateinit var crime: Crime
+        private val titleTextView:TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView:TextView=itemView.findViewById(R.id.crime_date)
+        init {
+            itemView.setOnClickListener(this)
+        }
+        fun bind(crime:Crime){
+            this.crime=crime
+            titleTextView.text=this.crime.title
+            dateTextView.text=this.crime.data.toString()
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context,"${crime.title}pressed!",Toast.LENGTH_SHORT).show()
+        }
     }
     private inner class CrimeAdapter(var crimes:List<Crime>):RecyclerView.Adapter<CrimeHolder>(){
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-            val view = layoutInflater.inflate(R.layout.list_item_crime,parent,false)
-            return CrimeHolder(view)
+            val view0 = layoutInflater.inflate(R.layout.list_item_crime,parent,false)
+            val view1 = layoutInflater.inflate(R.layout.list_item_crime1,parent,false)
+            if(viewType==0)
+            return CrimeHolder(view0)
+            else return CrimeHolder(view1)
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            val crime=crimes[position]
+            if(crime.requiresPolice){
+                return 1
+            }else{
+                return 0
+            }
         }
 
         override fun getItemCount(): Int {
@@ -38,10 +64,7 @@ class CrimeListFragment:Fragment() {
 
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
-            holder.apply {
-                titleTextView.text=crime.title
-                dateTextView.text=crime.data.toString()
-            }
+            holder.bind(crime)
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
