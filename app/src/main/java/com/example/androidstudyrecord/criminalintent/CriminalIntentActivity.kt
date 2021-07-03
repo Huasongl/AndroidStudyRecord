35d827698fccb20c66aaf3817ba26a462ec491a0
yaohua <1436385860@qq.com>
2021-07-03 22:40:36 +0800

    Implement a Fragment that contain Recycler List

--- app/src/main/java/com/example/androidstudyrecord/criminalintent/CriminalIntentActivity.kt
+++ app/src/main/java/com/example/androidstudyrecord/criminalintent/CriminalIntentActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidstudyrecord.R
 import com.example.androidstudyrecord.databinding.ActivityCriminalIntentBinding
 
 class CriminalIntentActivity : AppCompatActivity() {
@@ -12,6 +13,14 @@ class CriminalIntentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         view= ActivityCriminalIntentBinding.inflate(layoutInflater)
         setContentView(view.root)
+        val currentFragment=supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(currentFragment==null){
+            val fragment=CrimeListFragment.newInstance()
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit()
+        }
     }
     companion object{
         fun actionStart(context:Context){
---
+++ app/src/main/java/com/example/androidstudyrecord/criminalintent/readme.md
@@ -0,0 +1,13 @@
+# recycler and Fragment List the implement
+
+## Fragment implement 
+
+- create a container layout for Fragment 
+- create a Fragment class 
+- create a ViewModel for store data class list  
+- create support fragment manager in Activity that is transaction
+
+## Recycler implement 
+
+- create a ViewHolder 
+- create a Adapter
