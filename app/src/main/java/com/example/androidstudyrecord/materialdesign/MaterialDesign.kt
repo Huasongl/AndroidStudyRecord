e06277388543f5dbcf5c33d67f0169d068a85782
yaohua <1436385860@qq.com>
2021-07-06 22:53:46 +0800

    add a menu in toolbar

--- app/src/main/java/com/example/androidstudyrecord/materialdesign/MaterialDesign.kt
+++ app/src/main/java/com/example/androidstudyrecord/materialdesign/MaterialDesign.kt
@@ -1,13 +1,13 @@
 package com.example.androidstudyrecord.materialdesign
-
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextThemeWrapper
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.androidstudyrecord.R
-
 class MaterialDesign : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +15,21 @@ class MaterialDesign : AppCompatActivity() {
         val toolbar:Toolbar=findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.backup->Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show()
+            R.id.delete->Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show()
+            R.id.settings->Toast.makeText(this,"You clicked Settings",Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
     companion object{
         fun actionStart(context:Context){
             val intent=Intent(context,MaterialDesign::class.java)
--- app/src/main/java/com/example/androidstudyrecord/materialdesign/readme.md
+++ app/src/main/java/com/example/androidstudyrecord/materialdesign/readme.md
@@ -1,2 +1,21 @@
 # Material Design Test
 
+## Add Toolbar in Activity
+
+- add a style design in themes.xml
+- assign theme in activity
+- add a toolbar view in layout
+  - set theme
+  - set background
+  - set menu theme
+- setSupportActionBar 
+
+### Add menu in Toolbar 
+
+- create menu.xml
+  - add item
+    - set icon,id,title,action
+- onCreateOptionsMenu
+  - menuInflater.inflate
+- onOptionItemSelected
+  - when(item)
